218a9cb8208aab4c7e41533897aa91e997c74df9
J-Boogie <jackr353@gmail.com>
2021-02-03 16:32:24 -0600

    add soodo

--- server.js
+++ server.js
@@ -111,3 +111,30 @@ function searchTopArtists() {
 //         }
 //       });
 //   }
+
+
+// /Build a command-line application that at a minimum allows the user to:
+
+// * Add departments, roles, employees
+
+                                                    // addDepartment(), addRole(), addEmployee()
+
+// * View departments, roles, employees
+
+                                                    // viewDepartment(), viewRoles(), viewEmployees()
+
+// * Update employee roles
+
+                                                    // updateEmployees()
+
+// Bonus points if you're able to:
+
+// * Update employee managers
+
+// * View employees by manager
+
+// * Delete departments, roles, and employees   
+
+                                                                //deleteDepartment(), deleteRole(), deleteEmployee()
+
+// * View the total utilized budget of a department -- ie the combined salaries of all employees in that department
